8e639448d089da24273ea2334b1998f2c641214c
atakurt034 <69969985+atakurt034@users.noreply.github.com>
2021-01-16 17:17:59 +0800

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react'
+
 import Layout from '../components/layout/Layout'
 import About from '../screens/about/AboutScreen'
 import Stacks from '../screens/stacks/StacksScreen'
@@ -7,23 +9,38 @@ import { GetStaticProps } from 'next'
 import { getProject } from './api/projects'
 
 import { ContactProvider } from './api/lib/contactContext'
+import { ModalLoader } from '../components/ModalLoader'
+
+import { useRouter } from 'next/router'
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const projects = await getProject()
   return { props: { projects } }
 }
 
-export default function Home(
-  props: JSX.IntrinsicAttributes & { projects: any }
-) {
+interface Props {
+  projects: {}[]
+}
+
+const Home: React.FC<Props> = (props) => {
+  const router = useRouter()
+
   return (
-    <Layout>
-      <About />
-      <Stacks />
-      <Project {...props} />
-      <ContactProvider>
-        <Contact />
-      </ContactProvider>
-    </Layout>
+    <>
+      {router.isFallback ? (
+        <ModalLoader />
+      ) : (
+        <Layout>
+          <About />
+          <Stacks />
+          <Project {...props} />
+          <ContactProvider>
+            <Contact />
+          </ContactProvider>
+        </Layout>
+      )}
+    </>
   )
 }
+
+export default Home
